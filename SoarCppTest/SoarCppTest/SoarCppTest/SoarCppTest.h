536b605788cf7798e26946a874d9ea6cb66e039d
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2023-01-31 16:16:36 -0300

    clear other tests

--- SoarCppTest/SoarCppTest/SoarCppTest/SoarCppTest.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef SOARCPPTEST_EXPORTS
-#define SOARCPPTEST_API __declspec(dllexport)
-#else
-#define SOARCPPTEST_API __declspec(dllimport)
-#endif
-
-extern "C" SOARCPPTEST_API int soar_test();
